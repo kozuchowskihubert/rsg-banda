5b8f2a782d2da8c3b2fcb1ab1a1f10c7f355626c
Hubert Kozuchowski <hubertkozuchowski1337@gmail.com>
2025-12-16 04:06:44 +0100

    🔧 Fix api/index.js basedir

--- api/index.js
+++ api/index.js
@@ -1,2 +1,11 @@
 // Vercel serverless function entry point
-module.exports = require('./app/app');
+const path = require('path');
+
+// Ustaw __dirname dla względnych importów
+global.__basedir = path.resolve(__dirname, '..');
+
+// Import Express app
+const app = require('../app/app');
+
+// Export jako serverless function
+module.exports = app;
